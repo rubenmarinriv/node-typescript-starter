40c8b70be32c817aaa2d9975ea93c9acd5873278
Rubén Marín Rivarés <rubenmarinriv@gmail.com>
2024-02-28 20:12:12 +0100

    Configure eslint-config-airbnb-base

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,35 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "es2021": true,
-        "node": true
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "plugins": ["@typescript-eslint"],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+  },
+};
